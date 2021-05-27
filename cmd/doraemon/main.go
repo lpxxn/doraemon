1eaf3d7accb87aa285c9fa4583208fa24a76f8ed
lpxxn <mi_duo@live.com>
2021-05-27 21:12:53 +0800

    feat: custom command

--- cmd/doraemon/main.go
+++ cmd/doraemon/main.go
@@ -56,6 +56,7 @@ func main() {
 			Name:   "sshCompleter",
 			Target: getSSHCompleter,
 		}),
+		//fx.Provide(NewSSHPrompt),
 		fx.Populate(&sd, &lc),
 		fx.Invoke(RunSSHCommand))
 }
@@ -63,6 +64,49 @@ func main() {
 type sshCmdParam struct {
 	dig.In
 	Completer prompt.Completer `name:"sshCompleter"`
+}
+
+type ICommand interface {
+	Exec() error
+}
+
+type sshPromptInfo struct {
+	Completer prompt.Completer `name:"sshCompleter"`
+}
+
+var sshPrompt *sshPromptInfo
+
+func NewSSHPrompt(param sshCmdParam) {
+	sshPrompt = &sshPromptInfo{Completer: param.Completer}
+}
+
+func (s *sshPromptInfo) Exec() error {
+exitCmd:
+	for {
+		utils.SendMsg(true, "Hi!", "Please select a command.", utils.Yellow, false)
+		//fmt.Println("Please select a command.")
+		cmdName := prompt.Input(consolePrefix, s.Completer, prompt.OptionAddKeyBind(prompt.KeyBind{
+			Key: prompt.ControlC,
+			Fn: func(buffer *prompt.Buffer) {
+				fmt.Println("👋👋👋 bye ~")
+				os.Exit(0)
+			},
+		}))
+		if _, ok := existCommand[cmdName]; ok {
+			fmt.Println("👋👋👋 bye ~")
+			break exitCmd
+		}
+		if openConfigDir == cmdName {
+			if err := config.OpenConfDir(); err != nil {
+				fmt.Println(err)
+			}
+			continue
+		}
+		if err := startSSHShell(cmdName); err != nil {
+			fmt.Println(err)
+		}
+	}
+	return nil
 }
 
 func RunSSHCommand(param sshCmdParam) {
