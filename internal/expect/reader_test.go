d72675972af0d914a6a4561b49efd9c66d3e52b0
lipeng <lipeng@meican.com>
2024-04-15 10:58:02 +0800

    feat: test io pip

--- internal/expect/reader_test.go
+++ internal/expect/reader_test.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"sync"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestReaderLease1(t *testing.T) {
@@ -34,18 +36,63 @@ func TestPip1(t *testing.T) {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		r := bufio.NewReader(reader)
-		for {
-			b, err := r.ReadByte()
-			if err != nil {
-				t.Logf("read err %#v", err)
-				return
-			}
-			t.Logf("read: %s", string(b))
-		}
+		writer.Write([]byte("abc"))
+		writer.Write([]byte("def"))
+		writer.Close()
+
 	}()
-	writer.Write([]byte("abc"))
-	writer.Write([]byte("def"))
-	writer.Close()
+
+	r := bufio.NewReader(reader)
+	for {
+		b, err := r.ReadByte()
+		if err != nil {
+			t.Logf("read err %#v", err)
+			return
+		}
+		t.Logf("read: %s", string(b))
+	}
+	wg.Wait()
+}
+
+func TestPip2(t *testing.T) {
+	reader, writer := io.Pipe()
+	defer func() {
+		reader.Close()
+		//writer.Close()
+	}()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		newReader, newWriter := io.Pipe()
+		defer newReader.Close()
+		defer newWriter.Close()
+
+		go func() {
+			defer writer.Close()
+			_, err := io.Copy(writer, newReader)
+			t.Logf("io copy err: %#v", err)
+		}()
+
+		//defer newWriter.Close()
+		_, err := newWriter.Write([]byte("hello"))
+		assert.Nil(t, err)
+		_, err = newWriter.Write([]byte("world"))
+		assert.Nil(t, err)
+		//time.Sleep(5)
+
+		//newWriter.Close()
+
+	}()
+
+	r := make([]byte, 5)
+	for {
+		b, err := reader.Read(r)
+		if err != nil {
+			t.Logf("read err %#v", err)
+			return
+		}
+		t.Logf("read: %s, count: %d", string(r), b)
+	}
 	wg.Wait()
 }
